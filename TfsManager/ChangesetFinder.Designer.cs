f262032872bd33514ae267ce7b4b1781bd62e375
bahadir.akcora <bahadir.akcora@02483276-PN27>
2017-03-24 10:17:15 +0300

    Add project files.

---
+++ TfsManager/ChangesetFinder.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TfsManager
+{
+    partial class ChangesetFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 107);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 169);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Get Changesets";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Changeset List";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "TFS URL";
+            // 
+            // ChangesetFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 313);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ChangesetFinder";
+            this.Text = "ChangesetFinder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TfsManager/ChangesetFinder.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.TeamFoundation;
+using Microsoft.TeamFoundation.Framework.Client;
+using System.Linq;
+
+namespace TfsManager
+{
+    public partial class ChangesetFinder : Form
+    {
+        public ChangesetFinder()
+        {
+            InitializeComponent();
+        }
+        public class ChangesetItem
+        {
+            public string Comment { get; set; }
+            public long WorkItemId { get; set; }
+            public long ChangesetId { get; set; }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+
+            if (string.IsNullOrEmpty(textBox2.Text))
+                return;
+            richTextBox1.Text = string.Empty;
+            var tfsUri = new Uri(textBox1.Text);
+
+            var tpc = new TfsTeamProjectCollection(tfsUri);
+
+            var workItemStore = (WorkItemStore)tpc.GetService(typeof(WorkItemStore));
+            var configurationServer = TfsConfigurationServerFactory.GetConfigurationServer(tfsUri);
+            var tpcService = configurationServer.GetService<ITeamProjectCollectionService>();
+
+            var arr = textBox2.Text.Split(',');
+
+            List<long> results = new List<long>();
+            foreach (string s in arr)
+            {
+                long val;
+
+                if (long.TryParse(s.Trim(), out val))
+                {
+                    results.Add(val);
+                }
+            }
+
+            long[] workItemList = results.Distinct().ToArray();
+
+            var workItems = new List<WorkItem>();
+
+            foreach (var id in workItemList)
+            {
+                var wi = workItemStore.GetWorkItem((int)id);
+                if (wi != null)
+                {
+                    workItems.Add(wi);
+                }
+            }
+
+            var list = new List<ChangesetItem>();
+
+            foreach (var workItem in workItems)
+            {
+                var cs = GetChangesetList(workItem, workItemStore);
+
+                if (cs != null && cs.Count > 0)
+                {
+                    list.AddRange(cs);
+                }
+            }
+
+            list = list.OrderBy(x => x.ChangesetId).Distinct().ToList();
+            richTextBox1.Text += 0 + "\t" + "ChangesetId" + "\t" + "WorkItemId" + "\t" + "Comment" + System.Environment.NewLine;
+            for (var i = 0; i < list.Count; i++)
+            {
+                richTextBox1.Text += i + 1 + "\t" + list[i].ChangesetId + "\t" + list[i].WorkItemId + "\t" + list[i].Comment + System.Environment.NewLine;
+            }
+        }
+
+        private List<ChangesetItem> GetChangesetList(WorkItem workItem, WorkItemStore ws)
+        {
+            var list = new List<ChangesetItem>();
+            foreach (var link in workItem.Links)
+            {
+                if (link == null)
+                    continue;
+
+                if (link is ExternalLink)
+                {
+                    string externalLink = ((ExternalLink)link).LinkedArtifactUri;
+                    var artifact = LinkingUtilities.DecodeUri(externalLink);
+
+                    if (artifact.ArtifactType == "Changeset")
+                    {
+                        long csId = 0;
+
+                        if (long.TryParse(artifact.ToolSpecificId, out csId))
+                        {
+                            var csItem = new ChangesetItem();
+                            csItem.ChangesetId = csId;
+                            csItem.WorkItemId = workItem.Id;
+                            csItem.Comment = "";
+                            list.Add(csItem);
+                        }
+                    }
+                }
+                else if (link is RelatedLink)
+                {
+
+                    var rLink = ((RelatedLink)link);
+
+                    if (rLink.LinkTypeEnd.Name == "Child")
+                    {
+                        var id = ((RelatedLink)rLink).RelatedWorkItemId;
+
+                        var childWorkItem = ws.GetWorkItem(id);
+
+                        if (childWorkItem != null)
+                        {
+                            var childCs = GetChangesetList(childWorkItem, ws);
+
+                            if (childCs != null && childCs.Count > 0)
+                            {
+                                list.AddRange(childCs);
+                            }
+                        }
+                    }
+
+                }
+            }
+            return list;
+        }
+
+        private WorkItemCollection GetWorkItemById(WorkItemStore workItemStore, long workItemId)
+        {
+            var queryResults = workItemStore.Query(
+            string.Format("Select [Title] " +
+            "From WorkItems " +
+            "Where [ID] = '{0}' ", workItemId));
+            return queryResults;
+        }
+
+    }
+}
